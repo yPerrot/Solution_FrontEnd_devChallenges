14a40f8d8ff93bf8a0332a54b811c67b0489718c
yPerrot <y.perrot@outlook.fr>
2022-09-08 22:46:10 +0200

    feat: Add components

--- quote-generator/src/App.tsx
+++ quote-generator/src/App.tsx
@@ -1,10 +1,72 @@
+import { useEffect, useState } from 'react';
+import { Quote, getRandomQuote, getAuthorQuotes } from './api/QuoteAPI';
+import QuoteJSX from './components/Quote';
 import './App.scss';
 
 function App() {
+
+  const [author, setAuthor] = useState('');
+  const [quotes, setQuotes] = useState<Quote[] |null>(null);
+
+  useEffect(() => {
+    if (!quotes) {
+      getQuotes();
+    }
+  }, []);
+
+  const getQuotes = async (quoteAuthor?: string) => {
+    let quotesList: Quote[] = [];
+
+    if (quoteAuthor) {
+      quotesList = await getAuthorQuotes(quoteAuthor);
+    } else {
+      const randomQuote = await getRandomQuote(); 
+      quotesList = [ randomQuote ];
+    }
+
+    setQuotes(quotesList);
+  };
+
+  const handleRandomQuote = () => {
+    getQuotes();
+    setAuthor('');
+  };
+
+  const handleAuthorQuotes = () => {
+    if (quotes) {
+      getQuotes(quotes[0].quoteAuthor);
+      setAuthor(quotes[0].quoteAuthor);
+    }
+  };
+
+  const quotsToJSX = () => {
+    return quotes?.map( (q, index) => {
+      return (
+        <QuoteJSX text={q.quoteText} key={index} />
+      );
+    });
+  };
+
   return (
-    <div className="App">
-      My App
-    </div>
+    <>
+      <header>
+        <button onClick={handleRandomQuote} className='random-btn'>
+          <span>random</span>
+          <span className="material-icons">autorenew</span>
+        </button>
+      </header>
+
+      <main>
+        <h1>{author}</h1>
+        {quotes && quotsToJSX()}
+        {author === '' && quotes != null && 
+          <div onClick={handleAuthorQuotes}>
+            <span>{quotes[0].quoteAuthor}</span>
+            <span className="material-icons">east</span>
+          </div>
+        }
+      </main>
+    </>
   );
 }
 
---
+++ quote-generator/src/components/Quote.tsx
@@ -0,0 +1,15 @@
+
+interface QuoteProps {
+    text: string,
+}
+
+const Quote = ({text}: QuoteProps) => {
+
+  return (
+    <div className="quote">
+      {text}
+    </div>
+  );
+};
+
+export default Quote;
